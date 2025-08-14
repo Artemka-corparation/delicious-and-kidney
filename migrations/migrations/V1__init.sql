42165ce6ec92c48ac9da475e153d966769559e45
danil <daniltikhonov702@gmail.com>
2025-08-14 10:02:26 +0300

    докер и первая миграция через флайвей

---
+++ migrations/migrations/V1__init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       name TEXT NOT NULL,
+                       email TEXT UNIQUE NOT NULL,
+                       password_hash TEXT NOT NULL,
+                       created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE categories (
+                            id SERIAL PRIMARY KEY,
+                            name TEXT NOT NULL
+);
+
+CREATE TABLE products (
+                          id SERIAL PRIMARY KEY,
+                          name TEXT NOT NULL,
+                          description TEXT,
+                          price NUMERIC(10,2) NOT NULL,
+                          image_url TEXT,
+                          category_id INT REFERENCES categories(id)
+);
+
+CREATE TABLE orders (
+                        id SERIAL PRIMARY KEY,
+                        user_id INT REFERENCES users(id),
+                        status TEXT NOT NULL,
+                        total_price NUMERIC(10,2) NOT NULL,
+                        created_at TIMESTAMP DEFAULT now()
+);
+
