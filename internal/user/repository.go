8eb8f264d36713115611f26dfb3dde50f59ab168
danil <daniltikhonov702@gmail.com>
2025-08-17 19:19:18 +0300

    настроил структуру, подключение к бд, круд методы,

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,54 @@
 package user
+
+import "delicious-and-kidney/pkg/db"
+
+type UserRepository struct {
+	database *db.Db
+}
+
+func NewUserRepository(database *db.Db) *UserRepository {
+	return &UserRepository{database: database}
+}
+func (repo *UserRepository) Create(user *User) (*User, error) {
+	result := repo.database.Create(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
+}
+func (repo *UserRepository) FindById(Id int) (*User, error) {
+	var user User
+	result := repo.database.First(&user, Id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+
+func (repo *UserRepository) FindByEmail(email string) (*User, error) {
+	var user User
+	result := repo.database.Where("email = ?", email).First(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+func (repo *UserRepository) Update(user *User) (*User, error) {
+	result := repo.database.Save(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
+}
+func (repo *UserRepository) UpdateFields(id uint, updates map[string]interface{}) error {
+	result := repo.database.Model(&User{}).Where("id = ?", id).Updates(updates)
+	return result.Error
+}
+func (repo *UserRepository) Delete(id uint) error {
+	result := repo.database.Delete(&User{}, id)
+	return result.Error
+}
+func (repo *UserRepository) HardDelete(id uint) error {
+	result := repo.database.Unscoped().Delete(&User{}, id)
+	return result.Error
+}
--- internal/user/types.go
+++ internal/user/types.go
@@ -1,1 +1,12 @@
 package user
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name         string `gorm:"not null" json:"name"`
+	Email        string `gorm:"unique" validate:"required,email"`
+	Phone        string `gorm:"not null" validate:"required,min=10"`
+	PasswordHash string `gorm:"not null" json:"-"`
+	Role         string `gorm:"default:customer;check:role IN ('customer','restaurant_owner','courier','admin')"`
+}
