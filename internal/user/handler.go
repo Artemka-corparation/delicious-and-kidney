2a51f3ad404ba9c5290273e11185e5f9e8216d08
danil <daniltikhonov702@gmail.com>
2025-08-18 16:11:00 +0300

    правки

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,1 +1,43 @@
 package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type UserHandler struct {
+	userRepo UserRepositoryInterface
+}
+
+func NewUserHandler(userRepo UserRepositoryInterface) *UserHandler {
+	return &UserHandler{userRepo: userRepo}
+}
+
+func (h *UserHandler) GetUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	user, err := h.userRepo.FindById(uint(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+func (h *UserHandler) CreateUser(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	createdUser, err := h.userRepo.Create(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data": createdUser})
+}
