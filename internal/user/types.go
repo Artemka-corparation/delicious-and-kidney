07bfd52c570e86e9e8e8179b2c3b5b792911601d
danil <daniltikhonov702@gmail.com>
2025-08-18 11:44:56 +0300

    правки

--- internal/user/types.go
+++ internal/user/types.go
@@ -1,12 +1,16 @@
 package user
 
-import "gorm.io/gorm"
+import "time"
 
 type User struct {
-	gorm.Model
-	Name         string `gorm:"not null" json:"name"`
-	Email        string `gorm:"unique" validate:"required,email"`
-	Phone        string `gorm:"not null" validate:"required,min=10"`
-	PasswordHash string `gorm:"not null" json:"-"`
-	Role         string `gorm:"default:customer;check:role IN ('customer','restaurant_owner','courier','admin')"`
+	Id            uint      `gorm:"primarykey"`
+	Name          string    `gorm:"not null" json:"name"`
+	Email         string    `gorm:"unique" validate:"required,email"`
+	Phone         string    `gorm:"size:20" json:"phone"`
+	PasswordHash  string    `gorm:"not null" json:"-"`
+	Role          string    `gorm:"default:customer;check:role IN ('customer','restaurant_owner','courier','admin')"`
+	EmailVerified bool      `gorm:"default:false"`
+	PhoneVerified bool      `gorm:"default:false"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
 }
