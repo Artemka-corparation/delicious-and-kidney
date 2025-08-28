219fa4663012b7eb75a104e84f8e866696b3440a
danil <daniltikhonov702@gmail.com>
2025-08-28 14:02:57 +0300

    реализовал основные методы для сервиса ресторана

--- cmd/restaurant-service/main.go
+++ cmd/restaurant-service/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"delicious-and-kidney/configs"
+	"delicious-and-kidney/internal/auth"
+	"delicious-and-kidney/internal/user"
+
+	"delicious-and-kidney/internal/restaurant"
+	"delicious-and-kidney/pkg/db"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	config := configs.LoadConfig()
+
+	database, err := db.NewDb(config)
+	if err != nil {
+		log.Fatal("Not to connect to database", err)
+	}
+	err = database.AutoMigrate(&restaurant.Restaurant{})
+	if err != nil {
+		log.Fatal("Not to auto migrate", err)
+	}
+
+	userRepo := user.NewUserRepository(database)
+
+	restaurantRepo := restaurant.NewRestaurantRepository(database)
+	restaurantService := restaurant.NewRestaurantService(restaurantRepo, userRepo)
+	authRepo := auth.NewAuthRepository(database)
+	jvtService := auth.NewJWTService(config.Auth.Secret)
+	authService := auth.NewAuthService(authRepo, userRepo, jvtService)
+	restaurantHandler := restaurant.NewRestaurantHandler(restaurantService)
+	router := gin.Default()
+	restaurantHandler.RegisterRoutes(router, authService)
+	router.Run(":8083")
+}
